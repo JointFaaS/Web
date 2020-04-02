3dadc4883c7585aeadda891d8931a2568e34381b
Veiasai <o206@qq.com>
2020-04-02 17:05:41 +0800

    import antd

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Menu, Icon, Form, Input, Layout, Button, Checkbox, Spin, Row, Col, Divider, Pagination, PageHeader, List, Card } from 'ant-design-vue'
+import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
 
-Vue.use(Menu)
-Vue.use(Icon)
-Vue.use(Form)
-Vue.use(Input)
-Vue.use(Layout)
-Vue.use(Button)
-Vue.use(Checkbox)
-Vue.use(Spin)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Divider)
-Vue.use(Pagination)
-Vue.use(PageHeader)
-Vue.use(List)
-Vue.use(Card)
+Vue.use(Antd)
 
 new Vue({
   router,
